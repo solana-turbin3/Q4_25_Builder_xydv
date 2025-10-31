9555c81bb2767149bbefd7dc8c3d6599825dc188
aditya yadav <dedomil@tuta.io>
2025-10-31 16:03:09 +0530

    update resolve_bet.rs

--- anchor-dice-game/programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
+++ anchor-dice-game/programs/anchor-dice-game-q4-25/src/instructions/resolve_bet.rs
@@ -1,7 +1,15 @@
-use anchor_lang::prelude::*;
-use solana_program::sysvar::instructions;
+use anchor_instruction_sysvar::Ed25519InstructionSignatures;
+use anchor_lang::{
+    prelude::*,
+    system_program::{transfer, Transfer},
+};
+use solana_program::{
+    ed25519_program,
+    hash::hash,
+    sysvar::instructions::{self, load_instruction_at_checked},
+};
 
-use crate::Bet;
+use crate::{errors::DiceError, Bet};
 
 #[derive(Accounts)]
 pub struct ResolveBet<'info> {
@@ -34,10 +42,90 @@ pub struct ResolveBet<'info> {
 
 impl<'info> ResolveBet<'info> {
     pub fn verify_ed25519_signature(&mut self, sig: &[u8]) -> Result<()> {
+        let ix = load_instruction_at_checked(0, &self.instruction_sysvar.to_account_info())?;
+
+        // now we need to check, ix program id eq ed25519 program, accounts are 0 and extract signatures
+        require_keys_eq!(
+            ix.program_id,
+            ed25519_program::ID,
+            DiceError::Ed25519Program
+        );
+
+        require_eq!(ix.accounts.len(), 0, DiceError::Ed25519Accounts);
+
+        let signatures = Ed25519InstructionSignatures::unpack(&ix.data)
+            .map_err(|_| DiceError::Ed25519Signature)?
+            .0;
+
+        require_eq!(signatures.len(), 1, DiceError::Ed25519Signature);
+
+        let signature = &signatures[0];
+
+        // now we need to check, data is present, pubkey signature and messages match?
+        require!(signature.is_verifiable, DiceError::Ed25519Header); // header nhi hai
+
+        require_keys_eq!(
+            signature.public_key.ok_or(DiceError::Ed25519Pubkey)?,
+            self.house.key(),
+            DiceError::Ed25519Pubkey
+        );
+
+        require!(
+            signature
+                .signature
+                .ok_or(DiceError::Ed25519Signature)?
+                .eq(sig),
+            DiceError::Ed25519Signature
+        );
+
+        require!(
+            signature
+                .message
+                .as_ref()
+                .ok_or(DiceError::Ed25519Signature)?
+                .eq(&self.bet.to_slice()),
+            DiceError::Ed25519Signature
+        );
+
         Ok(())
     }
 
     pub fn resolve_bet(&mut self, sig: &[u8], bumps: &ResolveBetBumps) -> Result<()> {
+        let hash = hash(sig).to_bytes();
+        let mut hash_16: [u8; 16] = [0; 16];
+        hash_16.copy_from_slice(&hash[0..16]);
+        let lower = u128::from_le_bytes(hash_16);
+        hash_16.copy_from_slice(&hash[16..32]);
+        let upper = u128::from_le_bytes(hash_16);
+        let roll = lower.wrapping_add(upper).wrapping_rem(100) as u8 + 1;
+
+        // todo: add house fees
+        if self.bet.roll < roll {
+            let payout = (self.bet.amount as u128)
+                .checked_mul(10000)
+                .ok_or(DiceError::Overflow)?
+                .checked_div(self.bet.roll as u128 - 1)
+                .ok_or(DiceError::Overflow)?
+                .checked_div(100)
+                .ok_or(DiceError::Overflow)? as u64;
+
+            let accounts = Transfer {
+                from: self.vault.to_account_info(),
+                to: self.player.to_account_info(),
+            };
+
+            let signer_seeds: &[&[&[u8]]] =
+                &[&[b"vault", &self.house.key().to_bytes(), &[bumps.vault]]];
+
+            let ctx = CpiContext::new_with_signer(
+                self.system_program.to_account_info(),
+                accounts,
+                signer_seeds,
+            );
+
+            transfer(ctx, payout)?;
+        }
+
         Ok(())
     }
 }
